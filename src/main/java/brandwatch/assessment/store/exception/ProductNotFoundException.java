dc39c9ecc225577df87c09c7fe6171a2ddd4d0d2
MartinKal <martin.kalaydzhiev@gmail.com>
2023-04-13 10:10:37 +0300

    deleted unnecessary stuff

--- src/main/java/brandwatch/assessment/store/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package brandwatch.assessment.store.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/brandwatch/assessment/store/exception/ProductOutOfStockException.java
+++
@@ -1,7 +0,0 @@
-package brandwatch.assessment.store.exception;
-
-public class ProductOutOfStockException extends RuntimeException {
-    public ProductOutOfStockException(String message) {
-        super(message);
-    }
-}
